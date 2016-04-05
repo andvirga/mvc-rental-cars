c3ae0f395550e4b97c18554e109a4e2723462278
andres.virga <andres.virga@aajtech.com>
2016-04-05 11:56:40 -0300

    Initial Commit

---
+++ mvc-rental-cars/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc_rental_cars.Startup))]
+namespace mvc_rental_cars
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
